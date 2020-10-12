f7ed6114cd96fd45339bf2dd83f6d7aaf8f335fd
Padmavathi Betala <58491043+vempadiP@users.noreply.github.com>
2020-10-13 00:23:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+"""Author : Vidhya Gayathri
+Date : 08/10/2019
+Program : Main file that is responsible for the app to run"""
 
+import tkinter as tk
+from src.import_resumeGUI import ImportResumeGUI
+
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    obj = ImportResumeGUI(root)
+    root.mainloop()
